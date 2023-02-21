f32d9914e4311bfee911f74501ad423039ff9659
Rizvan <rizvan@MacBook-Air.fritz.box>
2023-02-21 18:24:50 +0100

    restaurant project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LongMenu from './components/longMenu';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  h1 {
+    color: #000;
+    background-color: red;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <h1 className="logo">Logo</h1>
+        <LongMenu />
+      </Header>
+      <section>
+        <h2>Hallo</h2>
+      </section>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    @font-face {
+      font-family: Poppins;
+      src: url('../resource/fonts/Poppins-Italic.ttf');
+    }
+    font-size: 10px;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
